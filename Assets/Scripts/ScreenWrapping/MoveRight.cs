26a1714651f608a1ddaf6b3b75f746936f787cb0
JStickles <josiah@grtbooks.com>
2018-11-03 00:56:44 -0400

    started work on screen wrapping

--- Assets/Scripts/ScreenWrapping/MoveRight.cs
+++ Assets/Scripts/ScreenWrapping/MoveRight.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class MoveRight : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private GameObject player;
+    private GameObject rightSide;
+    private GameObject leftSide;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+        rightSide = GameObject.FindWithTag("rightMapSide");
+        leftSide = GameObject.FindWithTag("leftMapSide");
+    }
 	
 	// Update is called once per frame
 	void Update () {
