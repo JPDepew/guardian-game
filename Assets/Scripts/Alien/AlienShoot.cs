50b13a7a3f086850b7cfe89a64564c1eb6e6148d
JStickles <josiah@grtbooks.com>
2018-11-09 00:24:25 -0500

    Added basic alien shooting
    Alien would spawns bullets when coroutine is fixed. Bullets also need a move function.

---
+++ Assets/Scripts/Alien/AlienShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienShoot : MonoBehaviour {
+
+    private Vector2 direction;
+    private float distance;
+    private GameObject player;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+        distance = direction.magnitude;
+        if (distance < 10)
+        {
+            //StartCoroutine(alienShoot());
+        }
+    }
+
+    /*IEnumerator alienShoot()
+    {
+        direction.Normalize();
+        GameObject alienBullet = Instantiate(bullet, transform.position + new Vector3(direction.x * .3f, direction.y * .3f, 0), transform.rotation);
+    }*/
+}
