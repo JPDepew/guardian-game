c4531ca07e8d6e5cb0693619772e0484852e41a0
John Paul Depew <jpdepewjp@gmail.com>
2018-11-17 14:57:08 -0500

    Got instructions menu set up

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UI : MonoBehaviour {
+public class UI : MonoBehaviour
+{
 
     AudioSource[] audioSource;
+    public Animator Instructions;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         audioSource = GetComponents<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void Play()
     {
@@ -27,8 +30,9 @@ public class UI : MonoBehaviour {
     public void ShowInstructions()
     {
         audioSource[0].Play();
+        StartCoroutine(CycleInstructions());
     }
-    
+
     public void CloseInstructions()
     {
         audioSource[0].Play();
@@ -38,5 +42,43 @@ public class UI : MonoBehaviour {
     {
         audioSource[0].Play();
         Application.Quit();
+
+    }
+
+    IEnumerator CycleInstructions()
+    {
+        Instructions.gameObject.SetActive(true);
+        Instructions.Play("Instructions1");
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                break;
+            }
+            yield return null;
+        }
+        Instructions.Play("Instructions2");
+        Input.ResetInputAxes();
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                break;
+            }
+            yield return null;
+        }
+        Instructions.Play("Instructions3");
+        Input.ResetInputAxes();
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                break;
+            }
+            yield return null;
+        }
+        Instructions.Play("FadeOut");
+        yield return new WaitForSeconds(Instructions.GetCurrentAnimatorStateInfo(0).length);
+        Instructions.gameObject.SetActive(false);
     }
 }
